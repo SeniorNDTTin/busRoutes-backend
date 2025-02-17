a3c46c95c1828ee02a2d7c493fdd31b16d3f80a1
Nguyen Duong Trong Tin <tinht5667@gmail.com>
2025-02-17 22:57:10 +0700

    fix

--- api/v1/routes/admin/index.route.ts
+++ api/v1/routes/admin/index.route.ts
@@ -5,11 +5,8 @@ import configs from "../../../../configs/index.config";
 import districtRoutes from "./district.route";
 import wardRoutes from "./ward.route";
 import streetRoutes from "./street.route";
-<<<<<<< HEAD
 import busStopRoutes from "./busStop.route";
-=======
 import busRoutes from "./bus.route";
->>>>>>> 666fe30f4353e264ef37fa32068232ea3022a3fc
 
 const adminRouteV1 = (app: Express) => {
   const path = "/api/v1" + configs.prefixAdmin;
@@ -17,11 +14,8 @@ const adminRouteV1 = (app: Express) => {
   app.use(`${path}/districts`, districtRoutes);
   app.use(`${path}/wards`, wardRoutes);
   app.use(`${path}/streets`, streetRoutes);
-<<<<<<< HEAD
   app.use(`${path}/busStops`, busStopRoutes);
-=======
   app.use(`${path}/buses`, busRoutes);
->>>>>>> 666fe30f4353e264ef37fa32068232ea3022a3fc
 }
 
 export default adminRouteV1;
